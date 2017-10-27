2f035eb2727ad33b8146fcab182e44eb86812fa4
Robert O'Connor <robertoconnor@ip129.ip-176-31-1.eu>
2017-10-27 12:40:57 +0100

    Updated ViewController with initial setup. Added components to MainStoryboard Create Currency model class

--- Currency/ViewController.swift
+++ Currency/ViewController.swift
@@ -2,25 +2,199 @@
 //  ViewController.swift
 //  Currency
 //
-//  Created by Robert O'Connor on 27/10/2017.
+//  Created by Robert O'Connor on 18/10/2017.
 //  Copyright © 2017 WIT. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK Model holders
+    var currencyDict:Dictionary = [String:Currency]()
+    var currencyArray = [Currency]()
+    var baseCurrency:Currency = Currency.init(name:"EUR", rate:1, flag:"🇪🇺", symbol:"€")!
+    var lastUpdatedDate:Date = Date()
+    
+    var convertValue:Double = 0
+    
+    //MARK Outlets
+    //@IBOutlet weak var convertedLabel: UILabel!
+    
+    @IBOutlet weak var baseSymbol: UILabel!
+    @IBOutlet weak var baseTextField: UITextField!
+    @IBOutlet weak var baseFlag: UILabel!
+    @IBOutlet weak var lastUpdatedDateLabel: UILabel!
+    
+    @IBOutlet weak var gbpSymbolLabel: UILabel!
+    @IBOutlet weak var gbpValueLabel: UILabel!
+    @IBOutlet weak var gbpFlagLabel: UILabel!
+    
+    @IBOutlet weak var usdSymbolLabel: UILabel!
+    @IBOutlet weak var usdValueLabel: UILabel!
+    @IBOutlet weak var usdFlagLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // print("currencyDict has \(self.currencyDict.count) entries")
+        
+        // create currency dictionary
+        self.createCurrencyDictionary()
+        
+        // get latest currency values
+        getConversionTable()
+        convertValue = 1
+        
+        // set up base currency screen items
+        baseTextField.text = String(format: "%.02f", baseCurrency.rate)
+        baseSymbol.text = baseCurrency.symbol
+        baseFlag.text = baseCurrency.flag
+        
+        // set up last updated date
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "dd/MM/yyyy hh:mm a"
+        lastUpdatedDateLabel.text = dateformatter.string(from: lastUpdatedDate)
+        
+        // display currency info
+        self.displayCurrencyInfo()
+        
+        
+        // setup view mover
+        baseTextField.delegate = self
+        
+        self.convert(self)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-
+    
+    func createCurrencyDictionary(){
+        //let c:Currency = Currency(name: name, rate: rate!, flag: flag, symbol: symbol)!
+        //self.currencyDict[name] = c
+        currencyDict["GBP"] = Currency(name:"GBP", rate:1, flag:"🇬🇧", symbol: "£")
+        currencyDict["USD"] = Currency(name:"USD", rate:1, flag:"🇺🇸", symbol: "$")
+    }
+    
+    func displayCurrencyInfo() {
+        // GBP
+        if let c = currencyDict["GBP"]{
+            gbpSymbolLabel.text = c.symbol
+            gbpValueLabel.text = String(format: "%.02f", c.rate)
+            gbpFlagLabel.text = c.flag
+        }
+        if let c = currencyDict["USD"]{
+            usdSymbolLabel.text = c.symbol
+            usdValueLabel.text = String(format: "%.02f", c.rate)
+            usdFlagLabel.text = c.flag
+        }
+    }
+    
+    
+    func getConversionTable() {
+        //var result = "<NOTHING>"
+        
+        let urlStr:String = "https://api.fixer.io/latest"
+        
+        var request = URLRequest(url: URL(string: urlStr)!)
+        request.httpMethod = "GET"
+        
+        let indicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        indicator.center = view.center
+        view.addSubview(indicator)
+        indicator.startAnimating()
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: OperationQueue.main) { response, data, error in
+            
+            indicator.stopAnimating()
+            
+            if error == nil{
+                //print(response!)
+                
+                do {
+                    let jsonDict = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String:Any]
+                    //print(jsonDict)
+                    
+                    if let ratesData = jsonDict["rates"] as? NSDictionary {
+                        //print(ratesData)
+                        for rate in ratesData{
+                            //print("#####")
+                            let name = String(describing: rate.key)
+                            let rate = (rate.value as? NSNumber)?.doubleValue
+                            //var symbol:String
+                            //var flag:String
+                            
+                            switch(name){
+                            case "USD":
+                                //symbol = "$"
+                                //flag = "🇺🇸"
+                                let c:Currency  = self.currencyDict["USD"]!
+                                c.rate = rate!
+                                self.currencyDict["USD"] = c
+                            case "GBP":
+                                //symbol = "£"
+                                //flag = "🇬🇧"
+                                let c:Currency  = self.currencyDict["GBP"]!
+                                c.rate = rate!
+                                self.currencyDict["GBP"] = c
+                            default:
+                                print("Ignoring currency: \(String(describing: rate))")
+                            }
+                            
+                            /*
+                             let c:Currency = Currency(name: name, rate: rate!, flag: flag, symbol: symbol)!
+                             self.currencyDict[name] = c
+                             */
+                        }
+                        self.lastUpdatedDate = Date()
+                    }
+                }
+                catch let error as NSError{
+                    print(error)
+                }
+            }
+            else{
+                print("Error")
+            }
+            
+        }
+        
+    }
+    
+    @IBAction func convert(_ sender: Any) {
+        var resultGBP = 0.0
+        var resultUSD = 0.0
+        
+        if let euro = Double(baseTextField.text!) {
+            convertValue = euro
+            if let gbp = self.currencyDict["GBP"] {
+                resultGBP = convertValue * gbp.rate
+            }
+            if let usd = self.currencyDict["USD"] {
+                resultUSD = convertValue * usd.rate
+            }
+        }
+        //GBP
+        
+        //convertedLabel.text = String(describing: resultGBP)
+        
+        gbpValueLabel.text = String(format: "%.02f", resultGBP)
+        usdValueLabel.text = String(format: "%.02f", resultUSD)
+    }
+    
+    /*
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     
+     }
+     */
+    
+    
 }
 
